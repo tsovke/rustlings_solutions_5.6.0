178de55cc2bc6f427b386e4978d1e4569cb08fd1
tsovke <tsovke@gmail.com>
2023-10-31 23:07:12 +0800

    solutions of rustlings

---
+++ exercises/clippy/target/debug/deps/clippy3-3c4aa8bdb7d79190.d
@@ -0,0 +1,9 @@
+/home/xt/rustlings/exercises/clippy/target/debug/deps/clippy3-3c4aa8bdb7d79190.rmeta: clippy3.rs Cargo.toml
+
+/home/xt/rustlings/exercises/clippy/target/debug/deps/clippy3-3c4aa8bdb7d79190.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
---
+++ exercises/lifetimes/lifetimes3.rs
@@ -0,0 +1,22 @@
+// lifetimes3.rs
+//
+// Lifetimes are also needed when structs hold references.
+//
+// Execute `rustlings hint lifetimes3` or use the `hint` watch subcommand for a
+// hint.
+
+struct Book<'b> {
+    author: &'b str,
+    title: &'b str,
+}
+
+fn main() {
+    let name = String::from("Jill Smith");
+    let title = String::from("Fish Flying");
+    let book = Book {
+        author: &name,
+        title: &title,
+    };
+
+    println!("{} by {}", book.title, book.author);
+}
